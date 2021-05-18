9a6c3f244280388de69764d92a5e933ba4fb8f2f
fkymy <yuskefuku@gmail.com>
2021-05-18 21:14:21 +0900

    CPP Module 02 ex00-01

--- CPPModule02/ex01/Fixed.cpp
+++ CPPModule02/ex01/Fixed.cpp
@@ -1,17 +1,29 @@
 #include "Fixed.hpp"
+#include <cmath>
 
 Fixed::Fixed() : raw(0) {
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
+Fixed::Fixed(const int num) {
+    std::cout << "Int constructor called" << std::endl;
+    raw = num << b;
+}
+
+Fixed::Fixed(const float num) {
+    std::cout << "Float constructor called" << std::endl;
+    raw = roundf(num * (1 << b));
 }
 
 Fixed::Fixed(const Fixed& other) {
     std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
 
 Fixed& Fixed::operator=(const Fixed& other) {
     std::cout << "Assignation operator called" << std::endl;
@@ -21,11 +33,21 @@ Fixed& Fixed::operator=(const Fixed& other) {
 }
 
 int Fixed::getRawBits() const {
-    std::cout << "getRawBits member function called" << std::endl;
     return raw;
 }
 
 void Fixed::setRawBits(const int raw) {
-    std::cout << "setRawBits member function call" << std::endl;
     this->raw = raw;
+}
+
+int Fixed::toInt() const {
+    return roundf(raw >> b);
+}
+
+float Fixed::toFloat() const {
+    return (float)raw / (float)(1 << b);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f) {
+    return os << f.toFloat();
 }
--- CPPModule02/ex01/Fixed.hpp
+++ CPPModule02/ex01/Fixed.hpp
@@ -6,6 +6,8 @@
 class Fixed {
 public:
     Fixed();
+    Fixed(const int num);
+    Fixed(const float num);
     ~Fixed();
 
     Fixed(const Fixed& other);
@@ -14,9 +16,14 @@ public:
     int getRawBits() const;
     void setRawBits(int const raw);
 
+    float toFloat() const;
+    int toInt() const;
+
 private:
     int raw;
     static const int b = 8;
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
 
 #endif /* FIXED_HPP */
