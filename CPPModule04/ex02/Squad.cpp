691cc01aed84321a36f5a89bab2c0f0a5d165798
fkymy <yuskefuku@gmail.com>
2021-05-20 16:02:42 +0900

    setup Squad

---
+++ CPPModule04/ex02/Squad.cpp
@@ -0,0 +1,64 @@
+#include "Squad.hpp"
+
+Squad::Squad() : count(0), marines(0) {}
+
+Squad::Squad(const Squad& other) {
+    copyOther(other);
+}
+
+Squad& Squad::operator=(const Squad& other) {
+    if (this != &other) {
+        clear();
+        copyOther(other);
+    }
+    return *this;
+}
+
+void Squad::copyOther(const Squad& other) {
+    count = other.count;
+    marines = new ISpaceMarine*[count];
+    for (int i = 0; i < count; ++i)
+        marines[i] = other.getUnit(i)->clone();
+}
+
+void Squad::clear() {
+    for (int i = 0; i < count; ++i) delete marines[i];
+    delete[] marines;
+    count = 0;
+}
+
+Squad::~Squad() {
+    clear();
+}
+
+int Squad::getCount() const {
+    return count;
+}
+
+ISpaceMarine* Squad::getUnit(int N) const {
+    if (count == 0 || N < 0 || N >= count)
+        return 0;
+    return marines[N];
+}
+
+int Squad::push(ISpaceMarine* marine) {
+    ISpaceMarine** tmp;
+
+    if (marine == 0)
+        return count;
+    for (int i = 0; i < count; ++i) {
+        if (marines[i] == marine)
+            return count;
+    }
+
+    tmp = new ISpaceMarine*[count + 1];
+    for (int i = 0; i < count; ++i)
+        tmp[i] = marines[i];
+    tmp[count] = marine;
+
+    delete [] marines;
+    marines = tmp;
+
+    ++count;
+    return count;
+}
---
+++ CPPModule04/ex02/Squad.hpp
@@ -0,0 +1,28 @@
+#ifndef SQUAD_HPP
+#define SQUAD_HPP
+
+#include <iostream>
+
+#include "ISpaceMarine.hpp"
+#include "ISquad.hpp"
+
+class Squad: public ISquad
+{
+    public:
+        Squad();
+        Squad(const Squad& other);
+        Squad& operator=(const Squad& other);
+        ~Squad();
+
+        virtual int getCount() const;
+        virtual ISpaceMarine* getUnit(int) const;
+        virtual int push(ISpaceMarine*);
+
+    private:
+        int count;
+        ISpaceMarine** marines;
+        void copyOther(const Squad& other);
+        void clear();
+};
+
+#endif /* SQUAD_HPP */
