6a2f2897ac3971794bc0e248e8828d362d77d576
fkymy <yuskefuku@gmail.com>
2021-05-24 15:32:58 +0900

    finalize tree

--- CPPModule05/ex02/ShrubberyCreationForm.cpp
+++ CPPModule05/ex02/ShrubberyCreationForm.cpp
@@ -22,8 +22,14 @@ void ShrubberyCreationForm::execute(const Bureaucrat& b) const {
     std::ofstream output(getTarget() + "_shrubbery");
     if (!output.is_open()) throw ShrubberyCreationForm::FileException();
 
-    // TODO: Draw tree
-    output << "TREE!!! =========>>>" << std::endl;
+    int height = 25;
+    int width = height * 2 - 1;
+    for (int i = 1; i <= height; ++i) {
+        int fill = i * 2 - 1;
+        output << std::setfill(' ') << std::setw((width - fill) / 2) << "";
+        output << std::setfill('*') << std::setw(i * 2 - 1) << "" << std::endl;
+    }
+    output << std::setfill(' ') << std::setw(width / 2) << "|" << std::endl;
     output.close();
 }
 
