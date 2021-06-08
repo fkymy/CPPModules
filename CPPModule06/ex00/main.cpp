53b84979f2fe8bba5b864dc744491b0bca0388a1
fkymy <yuskefuku@gmail.com>
2021-06-08 23:23:13 +0900

    fuck this module

--- CPPModule06/ex00/main.cpp
+++ CPPModule06/ex00/main.cpp
@@ -1,5 +1,7 @@
 #include "Converter.hpp"
 
+// TODO: isnan is apparently C++11...
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cout << "Invalid arguments. Usage: ./convert [string]"
